1b040761dcdae732db31b1b9f6043d285be2aa87
duyyudus <duyyudus@gmail.com>
2020-01-10 17:17:21 +0700

    Update endpoints

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from chainalytic_viz import client, visualizer
 
 # ENDPOINT = 'localhost:5530'
-# ENDPOINT = '45.76.184.255:5530'  # production
-ENDPOINT = '45.76.184.255:5531'  # production-dev
+# ENDPOINT = '45.76.184.255:5530'  # app
+# ENDPOINT = '45.76.184.255:5531'  # app-dev
+ENDPOINT = '140.82.11.203:5531'  # production chainalytic-dev
 # ENDPOINT = '35.240.229.245:5530'  # dev
 
 if __name__ == '__main__':
     c = client.Client(ENDPOINT)
     v = visualizer.Visualizer(c)
 
-    check = 5
+    check = 3
 
     if check == 0:
         c.last_block_height('stake_history')
@@ -27,3 +28,4 @@ if __name__ == '__main__':
         c.abstention_stake()
     elif check == 7:
         v.show_stake_history(1)
+
